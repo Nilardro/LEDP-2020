5ce4fe42c14c170fb4b796e9fcb45f42ea72aa65
Nilardro <nilardro5@gmailcom>
2020-06-05 10:05:02 -0700

    Ledp 1st project

---
+++ border.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Css Border</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <style type="text/css">
+        .border{
+            border: 15px solid;
+            border-image: linear-gradient(red, yellow) 4;
+        }
+    </style>
+</head>
+<body>
+
+<div class="">
+    <h2 class="border">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, eius!</h2>
+</div>
+
+</body>
+
+</html>
